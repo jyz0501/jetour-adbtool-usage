19689744bbfceac86aa6cf81fc85b64bff79a1ee
jyz0501 <jyz_0501@live.cn>
2026-03-01 10:06:07 +0800

    使用 git pre-commit hook 自动更新版本号

--- update_version.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-COMMIT_COUNT=$(git rev-list --count HEAD)
-
-VERSION="v1.${COMMIT_COUNT}.0"
-
-sed -i '' "s/<span id=\"app-version\" style=\"font-size: 12px; color: #999;\">.*<\/span>/<span id=\"app-version\" style=\"font-size: 12px; color: #999;\">${VERSION}<\/span>/" index.html
-
-echo "版本号已更新为: ${VERSION}"
